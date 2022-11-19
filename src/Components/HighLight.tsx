a635883b3de1f1f5dc8fa26171225039f43b71a8
Cristhian Gomez <christian@bettertrucks.com>
2022-11-18 23:11:22 -0600

    First commit

---
+++ src/Components/HighLight.tsx
@@ -0,0 +1,18 @@
+import {
+    Box,
+    Center,
+    Container,
+    Wrap,
+    WrapItem,
+    Text,
+    Image,
+    VStack,
+    SimpleGrid,
+  } from "@chakra-ui/react";
+  // ...
+  
+  
+  
+  
+ 
+  
